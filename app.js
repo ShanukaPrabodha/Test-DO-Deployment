0cd046cc9762020c4ea1b5fde6ae989feae7f1e0
Shanuka Prabodha <shanukakariyawasam3@gmail.com>
2023-09-27 12:43:57 +0530

    updated commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express =require ('express');
 const app =express ();
 
 app.get('/' , (req,res)=>{
-    res.send('<h1>My Node App SPK</h1>');
+    res.send('<h1>My Node App SPK-Updated</h1>');
 });
 
 app.listen(5000, ()=>{
